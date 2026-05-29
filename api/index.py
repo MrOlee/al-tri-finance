c8c751c8a59e94460aae8ae88bf641bfa445c354
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-05-29 16:00:44 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -17,6 +17,10 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
@@ -29,5 +33,5 @@ def scanner():
 def reports():
     return render_template('reports.html')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
