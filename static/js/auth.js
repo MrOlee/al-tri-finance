cb97bf9b1a0ad5b0ef1eb6f00129ac321a0d4f7f
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-05-29 16:05:12 +0700

    Update auth.js

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,1 +1,29 @@
+function login(){
 
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    if(email === '' || password === ''){
+        alert('Isi email dan password')
+        return
+    }
+
+    alert('Login berhasil')
+
+    window.location.href = '/dashboard'
+}
+
+function register(){
+
+    const email = document.getElementById('register-email').value
+    const password = document.getElementById('register-password').value
+
+    if(email === '' || password === ''){
+        alert('Isi email dan password')
+        return
+    }
+
+    alert('Register berhasil')
+
+    window.location.href = '/login'
+}
