31f0b5266c490795a5d10f282ef7e7422c3a579d
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-05-29 16:52:57 +0700

    Update supabase.js

--- static/js/supabase.js
+++ static/js/supabase.js
@@ -1,2 +1,7 @@
 const SUPABASE_URL = 'https://kfrbbypfjzfxrhgvggmc.supabase.co'
 const SUPABASE_KEY = 'sb_publishable__bscQnB0XIrLX3w62vNOAQ_0hiqfwhy'
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+)
