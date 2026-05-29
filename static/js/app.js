e9fc55f7028120e181dfe4dbf3ab89f7b2c9e473
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-05-29 16:05:42 +0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,11 @@
+function saveTransaction(){
 
+    const note = document.getElementById('note').value
+
+    if(note === ''){
+        alert('Isi transaksi')
+        return
+    }
+
+    alert('Transaksi berhasil disimpan')
+}
