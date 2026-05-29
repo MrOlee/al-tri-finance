c680e49bf90a8c5285c2eadf7d0558ab41618e03
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-05-29 16:06:30 +0700

    Update scanner.js

--- static/js/scanner.js
+++ static/js/scanner.js
@@ -1,1 +1,16 @@
+async function scanReceipt(){
 
+    const file = document.getElementById('receipt').files[0]
+
+    if(!file){
+        alert('Pilih gambar struk')
+        return
+    }
+
+    const result = await Tesseract.recognize(
+        file,
+        'eng'
+    )
+
+    document.getElementById('result').innerText = result.data.text
+}
