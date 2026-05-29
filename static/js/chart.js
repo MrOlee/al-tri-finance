af25a6d76517111c273e0053e6fd6bd4c68723fb
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-05-29 16:06:07 +0700

    Update chart.js

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,1 +1,17 @@
+const ctx = document.getElementById('financeChart')
 
+if(ctx){
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr'],
+        datasets: [{
+            label: 'Pengeluaran',
+            data: [1200000, 900000, 1500000, 800000],
+            borderWidth: 1
+        }]
+    }
+})
+
+}
